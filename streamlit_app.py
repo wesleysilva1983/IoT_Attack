1a727c96ad2d8563c66af1da96f3401e15a0e5d1
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-04 13:00:14 +0000

    Version_1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+with open("graphic.py") as f:
+    exec(f.read())
+
+with open("model.py") as f:
+    exec(f.read())
