82c0c51fe91261514276592e1ee17d6da13d9ca8
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-05 19:16:39 +0000

    iotv11

--- matriz_confusao.py
+++ matriz_confusao.py
@@ -7,16 +7,14 @@ import pandas as pd
 # Matriz de Confusão do arquivo
 confusion_matrix = np.array([[37442, 0, 3], [0, 38286, 35], [67, 74, 34409]])
 
-# Título da aplicação
-st.title("Matriz de Confusão do Modelo RF")
-
-# Mostrar a Matriz de Confusão como tabela
-st.subheader("Matriz de Confusão - Tabela")
-df_cm = pd.DataFrame(confusion_matrix, index=[0, 1, 2], columns=[0, 1, 2])
-st.write(df_cm)
+# Título da imagem centralizado
+st.markdown("<p style='text-align: center;'><strong>Matriz de Confusão do Modelo - Random Forest </h3>", unsafe_allow_html=True)
+# Exibe a imagem
+st.image("MetricasModelo.png", use_column_width=True)
+# Fonte da imagem centralizada
+st.markdown("<p style='text-align: center;'><strong>Fonte:</strong> Do autor, 2024.</p>", unsafe_allow_html=True)
 
 # Exibir a Matriz de Confusão como um gráfico de calor
-st.subheader("Matriz de Confusão - Gráfico de Calor")
 plt.figure(figsize=(8, 6))
 sns.heatmap(df_cm, annot=True, fmt="d", cmap="Blues")
 st.pyplot(plt)
