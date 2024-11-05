a1dbdcba85743c71b8af1c3b7f56632884bdd414
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-05 19:20:10 +0000

    iotv11

--- matriz_confusao.py
+++ matriz_confusao.py
@@ -10,10 +10,9 @@ df_cm = pd.DataFrame(confusion_matrix, index=[0, 1, 2], columns=[0, 1, 2])
 
 # Título centralizado
 st.markdown("<p style='text-align: center;'><strong>Matriz de Confusão do Modelo - Random Forest </h3>", unsafe_allow_html=True)
-# Fonte centralizada
-st.markdown("<p style='text-align: center;'><strong>Fonte:</strong> Do autor, 2024.</p>", unsafe_allow_html=True)
-
 # Exibir a Matriz de Confusão como um gráfico de calor
 plt.figure(figsize=(8, 6))
 sns.heatmap(df_cm, annot=True, fmt="d", cmap="Blues")
 st.pyplot(plt)
+# Fonte centralizada
+st.markdown("<p style='text-align: center;'><strong>Fonte:</strong> Do autor, 2024.</p>", unsafe_allow_html=True)
