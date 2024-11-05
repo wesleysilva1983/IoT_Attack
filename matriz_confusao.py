8c52c2d49019e5694fb7ad9ee325d33b6226af50
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-05 19:19:03 +0000

    iotv10

--- matriz_confusao.py
+++ matriz_confusao.py
@@ -6,12 +6,11 @@ import pandas as pd
 
 # Matriz de Confusão do arquivo
 confusion_matrix = np.array([[37442, 0, 3], [0, 38286, 35], [67, 74, 34409]])
+df_cm = pd.DataFrame(confusion_matrix, index=[0, 1, 2], columns=[0, 1, 2])
 
-# Título da imagem centralizado
+# Título centralizado
 st.markdown("<p style='text-align: center;'><strong>Matriz de Confusão do Modelo - Random Forest </h3>", unsafe_allow_html=True)
-# Exibe a imagem
-st.image("MetricasModelo.png", use_column_width=True)
-# Fonte da imagem centralizada
+# Fonte centralizada
 st.markdown("<p style='text-align: center;'><strong>Fonte:</strong> Do autor, 2024.</p>", unsafe_allow_html=True)
 
 # Exibir a Matriz de Confusão como um gráfico de calor
