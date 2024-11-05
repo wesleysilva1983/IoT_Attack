a5fc9f4c356f2480d428286513f22f25220cccde
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-05 03:11:51 +0000

    IoT v5

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,45 @@
 import streamlit as st
 
+# Logo alinhada à esquerda no topo da página principal
+st.markdown(
+    """
+    <div style="display: flex; align-items: center;">
+        <img src="LogoUEL.jpg" alt="LogoUEL" width="50" style="margin-right: 100px;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("""
+### Pós-graduação em Ciência da Computação - UEL
+**Projeto:** Detecção de Ataques no Tráfego de Dispositivos IoT.
+            
+**Disciplina:** 2COP507 Reconhecimento de Padrões.
+                    
+**Docentes:** Dr.  Bruno Bogaz Zarpelão, Dr. Sylvio Barbon Junior.
+            
+**Discente:** Wesley Candido da Silva.
+            
+**Dataset:** N-BaIoT Dataset to Detect IoT Botnet Attacks
+
+**Link:** https://www.kaggle.com/datasets/mkashifn/nbaiot-dataset
+""")
+
+texto = """
+<p style="text-align: justify;">
+<b>Resumo:</b> O presente projeto tem como objetivo a predição de ataques em dispositivos IoT, utilizando o dataset "N-BaIoT Dataset to Detect IoT Botnet Attacks". Esse conjunto de dados abrange 115 atributos relacionados ao tráfego de rede desses dispositivos, incluindo informações detalhadas sobre o fluxo de rede de nove dispositivos IoT comerciais que foram submetidos a ataques DDoS, utilizando dois dos botnets mais comuns: Mirai e Gafgyt (também conhecido como BASHLITE). Entre os atributos do dataset, incluem-se informações como IP, jitter (variação temporal entre pacotes) e socket (portas TCP/UDP). A partir desses atributos, foram extraídas diversas estatísticas, incluindo peso, média, desvio padrão, raio, magnitude, covariância e coeficiente de correlação de Pearson, utilizadas para caracterizar o comportamento normal do tráfego e identificar anomalias associadas a atividades de botnets. Devido ao grande volume de dados, totalizando aproximadamente 1,7 GB, a análise e o desenvolvimento do modelo de detecção foram focados em três dispositivos IoT selecionados para o treinamento e teste do modelo. Posteriormente, para validação e verificação da capacidade de generalização, os dados dos demais dispositivos IoT foram utilizados para predição dos ataques. Os resultados foram satisfatórios, com o modelo sendo capaz de prever corretamente a ocorrência de ataques e identificar o tipo específico de ataque DDoS em execução.
+</p>
+"""
+st.markdown(texto, unsafe_allow_html=True)
+
+# Título da imagem centralizado
+st.markdown("<p style='text-align: center;'><strong>Arquitetura para coleta de dados</h3>", unsafe_allow_html=True)
+# Exibe a imagem
+st.image("Arquitetura.jpg", use_column_width=True)
+# Fonte da imagem centralizada
+st.markdown("<p style='text-align: center;'><strong>Fonte:</strong> Yair Meidan, 2018.</p>", unsafe_allow_html=True)
+
+
 with open("graphic.py") as f:
     exec(f.read())
 
